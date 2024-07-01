afda3de13f5b30515c7e26eec5a0fe7b4353429c
Jerson <jersoncaibog0423@gmail.com>
2024-07-01 16:05:22 +0800

    added qr reader

---
+++ app/reader/page.tsx
@@ -0,0 +1,48 @@
+'use client'
+import React, { useEffect } from 'react'
+import { PageHeader } from '@/components'
+import { useRouter } from 'next/navigation'
+import { Html5QrcodeScanner } from 'html5-qrcode'
+import { useState } from 'react'
+
+const QrScanner = () => {
+
+  const [scanResult, setScanResult] = useState('');
+  
+  useEffect(() => {
+    const scanner = new Html5QrcodeScanner('reader', {
+      qrbox: {
+        width: 250,
+        height: 250,
+      },
+      fps: 5,
+    }, false);
+  
+    scanner.render(success, error);
+  
+    function success(result: string): void {
+      scanner.clear();
+      setScanResult(result)
+    }
+  
+    function error(err: string): void {
+      console.warn(err)
+    }
+  }, []);
+
+  return (
+    <>
+      <PageHeader title='QR Reader' />
+      <div className='p-5 '>
+        <div>QrScanner</div>
+
+        {scanResult 
+        ? <div>Success: {scanResult}</div>
+        : <div id='reader'></div>
+        }
+      </div>
+    </>
+  )
+}
+
+export default QrScanner
