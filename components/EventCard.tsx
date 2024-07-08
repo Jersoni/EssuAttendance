7ffe4764f51b627befdb18f21e46b5d92b3c2e46
Jerson <jersoncaibog0423@gmail.com>
2024-07-08 10:48:09 +0800

    Update EventCard styling

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
-import { MdOutlineLocationOn } from "react-icons/md";
-import { TbLogin2 } from "react-icons/tb";
-import { TbLogout2 } from "react-icons/tb";
+import { TiLocation } from "react-icons/ti";
+import { FaClock } from "react-icons/fa6";
+import { IoIosArrowForward } from "react-icons/io";
 
 interface Event {
     id: number
@@ -22,8 +22,12 @@ const EventsCard: React.FC<{ eventData: ParsedEvent }> = ({ eventData }) => {
 
   return (
     <Link href={`/events/${eventData.id}`}>
-      <div className="bg-white shadow-md h-fit rounded-2xl mt-4 p-4">
-        <span className="event__title">{eventData.title}</span>
+      <div className="flex flex-col bg-white border border-gray-300 h-fit rounded-lg mt-4 p-4">
+        <div className='flex flex-row items-center justify-between'>
+          <span className="event__title">{eventData.title}</span>
+          <IoIosArrowForward size={20} className='opacity-70'/>
+        </div>
+        <span className='text-xs font-medium mt-2 text-gray-500'>Attendees</span>
         <div className='text-xs flex flex-wrap gap-1 mt-2'>
           <span className="event__class">BSBA</span>
           <span className="event__class">BSIT</span>
@@ -33,18 +37,18 @@ const EventsCard: React.FC<{ eventData: ParsedEvent }> = ({ eventData }) => {
           <span className="event__class">BSE</span>
           <span className="event__class">BSCE</span>
         </div>
-        <div className="mt-4">
-          <div className='flex flex-row items-center gap-2'>
-            <MdOutlineLocationOn size={16} className='opacity-80'/>
+        <div className="mt-5 flex flex-row justify-between">
+          <div className='flex flex-row items-center gap-1 w-fit'>
+            <TiLocation size={19} className='opacity-60 translate-y-[-1px]'/>
             <span className="event__info">{eventData.location}</span>
           </div>
-          <div className='flex flex-row items-center gap-2'>
-            <TbLogin2 size={16} className='opacity-80' />
-            <span className="event__info">{eventData.loginTime}</span>
-          </div>
-          <div className='flex flex-row items-center gap-2'>
-            <TbLogout2 size={16} className='opacity-80'/>
-            <span className="event__info">{eventData.logoutTime}</span>
+          <div className='flex flex-row items-center gap-2 w-fit'>
+            <FaClock size={15} className='opacity-60 translate-y-[1px]' />
+            <div>
+              <span className="event__info">{eventData.loginTime}</span>
+              <span className="event__info mx-1">-</span>
+              <span className="event__info">{eventData.logoutTime}</span>
+            </div>
           </div>
         </div>
       </div>
